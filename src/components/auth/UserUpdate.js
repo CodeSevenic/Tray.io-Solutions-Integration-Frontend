372fef752e20287b4381f9d3580dfae31a6a4899
CodeSevenic <sevenicbookings@gmail.com>
2023-01-12 18:38:14 +0200

    Tray Solutions Code Update

---
+++ src/components/auth/UserUpdate.js
@@ -0,0 +1,49 @@
+﻿import React from 'react';
+
+const UserUpdate = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Handle submit');
+  };
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+
+  const signupForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            type="text"
+            className="form-control"
+            placeholder="Enter your name"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            type="password"
+            className="form-control"
+            placeholder="Enter your password"
+          />
+        </div>
+        <div>
+          <div className="btn btn-primary">UPDATE</div>
+        </div>
+      </form>
+    );
+  };
+
+  return <>{signupForm()}</>;
+};
+
+export default UserUpdate;

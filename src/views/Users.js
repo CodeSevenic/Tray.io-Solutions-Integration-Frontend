149bbc5e21b8073c70f76983a48bb87827c4d7ef
CodeSevenic <sevenicbookings@gmail.com>
2023-01-14 13:11:53 +0200

    Tray Solutions Code Update

--- src/views/Users.js
+++ src/views/Users.js
@@ -1,12 +1,24 @@
 ﻿import React from 'react';
+import { UserIcon } from '../components/svgs';
 import ViewAdmin from '../components/ViewAdmin';
+import './Users.css';
 
 const Users = () => {
   return (
     <ViewAdmin>
-      <section>
-        <header>All Users</header>
-        <div className="users-container"></div>
+      <section className="user-section">
+        <h2>All Users</h2>
+        <div className="users-container">
+          <div className="solution-user">
+            <div className="user-icon">
+              <UserIcon />
+            </div>
+            <div className="solution-user-info">
+              <h3>Caleb Shongwe</h3>
+            </div>
+            <button className="btn-delete">DELETE</button>
+          </div>
+        </div>
       </section>
     </ViewAdmin>
   );

835d8381b6405ab1e84905c6d8c5b2f054839eb3
CodeSevenic <sevenicbookings@gmail.com>
2023-01-14 17:03:34 +0200

    Tray Solutions Code Update

--- src/views/Users.js
+++ src/views/Users.js
@@ -2,17 +2,20 @@
 import { deleteUser, getUsers } from '../api/solutions';
 import { UserIcon } from '../components/svgs';
 import ViewAdmin from '../components/ViewAdmin';
+import Loader from '../img/animated-gif.gif';
 import './Users.css';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
   const [userCount, setUserCount] = useState(0);
   const [user, setUser] = useState('');
-
   const [overlay, setOverlay] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [userId, setUserId] = useState(null);
 
   const [deleteSuccess, setDeleteSuccess] = useState(false);
+  const [message, setMessage] = useState('');
 
   console.log(overlay);
 
@@ -34,21 +37,30 @@ const Users = () => {
     } else {
       document.body.style.overflow = 'initial';
     }
-
     return () => {
       document.body.style.overflow = 'initial';
     };
   }, [overlay]);
 
-  const handleDeleteUser = (userId) => {
-    deleteUser(userId).then((results) => {
-      console.log(results);
+  const getUserId = (id) => {
+    setUserId(id);
+  };
+
+  const handleDeleteUser = () => {
+    setLoading(true);
+    deleteUser(userId).then(({ ok, body }) => {
+      if (ok) {
+        setDeleteSuccess(true);
+        setMessage('Delete Successful 😊');
+        setLoading(false);
+      } else {
+        setDeleteSuccess(true);
+        setMessage('Deletion Failed 😒');
+        setLoading(false);
+      }
+      console.log(ok, body);
       getUsers().then(({ ok, body }) => {
         const { edges } = body.results.data.users;
-
-        if (ok) {
-          setDeleteSuccess(false);
-        }
         if (edges) {
           setUsers(edges);
           setUserCount(edges.length);
@@ -79,11 +91,14 @@ const Users = () => {
     }
   };
 
-  const onDeleteBtn = (user) => {
+  const onDeleteBtn = (user, id) => {
     setUser(user);
+    setUserId(id);
     setDeleteModal(!deleteModal);
     setOverlay(!overlay);
   };
+
+  console.log(userId);
 
   const confirmDelete = () => {
     return (
@@ -93,11 +108,7 @@ const Users = () => {
         className={`confirmDelete ${overlay ? '' : 'remove-modal'} ${overlay ? '' : 'd-none'}`}
       >
         <div className={`form-wrapper ${deleteModal ? '' : 'slide-out'}`}>
-          {!deleteSuccess ? (
-            <h4>Do you really want to delete {user}?</h4>
-          ) : (
-            <h4>Delete Successful</h4>
-          )}
+          {!deleteSuccess ? <h4>Do you really want to delete {user}?</h4> : <h4>{message}</h4>}
           <form onSubmit={onSubmitHandler}>
             {!deleteSuccess ? (
               <>
@@ -105,7 +116,9 @@ const Users = () => {
                   <button onClick={closeModalHandler} className="btn-delete cancel">
                     Cancel
                   </button>
-                  <button className="btn-delete">Delete</button>
+                  <button onClick={() => handleDeleteUser(userId)} className="btn-delete">
+                    {loading ? <img src={Loader} alt="" /> : 'Delete'}
+                  </button>
                 </div>
               </>
             ) : (
@@ -138,7 +151,10 @@ const Users = () => {
                 <div className="solution-user-info">
                   <h3>{user.node.name}</h3>
                 </div>
-                <button onClick={() => onDeleteBtn(user.node.name)} className="btn-delete">
+                <button
+                  onClick={() => onDeleteBtn(user.node.name, user.node.id)}
+                  className="btn-delete"
+                >
                   DELETE
                 </button>
               </div>
